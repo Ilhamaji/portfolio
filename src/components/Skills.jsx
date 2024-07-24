29417fda6afdbdb9acf1165e687454721b66bd75
ilhamaji <iheadskull@gmail.com>
2024-07-25 02:58:36 +0700

    fix overflowed screen

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,24 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { useInView } from "framer-motion";
-import Html from "../assets/img/html.png";
-import Css from "../assets/img/css-3.png";
-import Js from "../assets/img/js.png";
+import Html from "../assets/img/html.webp";
+import Css from "../assets/img/css-3.webp";
+import Js from "../assets/img/js.webp";
 import Php from "../assets/img/php.png";
 import Sql from "../assets/img/sql.png";
-import Rc from "../assets/img/react.png";
+import Rc from "../assets/img/react.svg";
+import Java from "../assets/img/java.png";
+import Tw from "../assets/img/tailwindcss.webp";
+import Fb from "../assets/img/flowbite.svg";
+import Fm from "../assets/img/framer-motion.webp";
+import Fireb from "../assets/img/firebase-icon.svg";
+import Vsc from "../assets/img/vscode.webp";
+import Git from "../assets/img/git.webp";
+import GitHub from "../assets/img/github.webp";
+import Npm from "../assets/img/npm.webp";
+import Vite from "../assets/img/vite.svg";
+import Figma from "../assets/img/figma.webp";
+import Netlify from "../assets/img/netlify-icon.svg";
 
 export default function Skills() {
   const [tab, setTab] = useState(1);
@@ -15,7 +27,10 @@ export default function Skills() {
 
   return (
     <>
-      <div className="flex w-auto h-10 bg-neutral-200 dark:bg-neutral-600 dark:text-white rounded-lg mx-10 mt-10">
+      <div className="title mt-5 text-center text-3xl font-bold dark:text-white">
+        Skills
+      </div>
+      <div className="flex w-auto h-10 bg-neutral-200 dark:bg-neutral-600 dark:text-white rounded-lg mx-10 mt-1">
         <button
           ref={skills}
           onClick={() => setTab(1)}
@@ -156,6 +171,25 @@ export default function Skills() {
                   <p className="text-center text-sm">SQL</p>
                 </div>
               </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.75,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[2000ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Java} alt="" />
+                  <p className="text-center text-sm">Java</p>
+                </div>
+              </motion.div>
             </motion.div>
           ) : tab === 2 ? (
             <div className="grid grid-cols-3 md:grid-cols-3 place-content-around gap-4">
@@ -178,9 +212,200 @@ export default function Skills() {
                   <p className="text-center text-sm">React JS</p>
                 </div>
               </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[1500ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Tw} alt="" />
+                  <p className="text-center text-sm">TailwindCSS</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[2000ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Fb} alt="" />
+                  <p className="text-center text-sm">Flowbite</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[1000ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Fm} alt="" />
+                  <p className="text-center text-sm">Framer</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[1500ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Fireb} alt="" />
+                  <p className="text-center text-sm">Firebase</p>
+                </div>
+              </motion.div>
             </div>
           ) : (
-            "Tech"
+            <div className="grid grid-cols-3 md:grid-cols-3 place-content-around gap-4">
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-1000"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Vsc} alt="" />
+                  <p className="text-center text-sm">VS Code</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[1500ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Figma} alt="" />
+                  <p className="text-center text-sm">Figma</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[2000ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Vite} alt="" />
+                  <p className="text-center text-sm">Vite</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-1000"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Git} alt="" />
+                  <p className="text-center text-sm">Git</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[1500ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={GitHub} alt="" />
+                  <p className="text-center text-sm">GitHub</p>
+                </div>
+              </motion.div>
+              <motion.div
+                animate={{
+                  opacity: [0, 1],
+                }}
+                transition={{
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                className={
+                  isInView
+                    ? "opacity-1 flex h-20 w-full rounded-lg bg-white dark:bg-neutral-500 dark:text-white duration-[2000ms]"
+                    : ""
+                }
+              >
+                <div className="m-auto block">
+                  <img className="w-10 h-10 m-auto" src={Npm} alt="" />
+                  <p className="text-center text-sm">NPM</p>
+                </div>
+              </motion.div>
+            </div>
           )}
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 export default function Home() {
   const [theme, setTheme] = useState("light");
@@ -20,11 +21,11 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-screen bg-neutral-100 dark:bg-neutral-900">
+      <div className="bg-neutral-100 dark:bg-neutral-900">
         <Nav theme={theme} themeHandler={themeHandler} />
         <Banner theme={theme} />
         <Skills />
-        <div className="h-96"></div>
+        <Projects />
       </div>
     </>
   );
